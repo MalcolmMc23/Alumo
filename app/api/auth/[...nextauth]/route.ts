4fcfe8f29351cf7531b4ef1fd51a63b5dced165c
Malcolm McDonald <malcolmemcdonald@gmail.com>
2025-02-23 22:53:52 -0800

    slashed!

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth"
-import { authOptions } from "@/lib/authOptions"
-
-// The new pattern: NextAuth returns a standard handler function.
-// We then export it for GET and POST:
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
